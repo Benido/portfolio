9cc58d3fb6bf2c0362716cd4618ec8dade9310e7
Benido <remi.besnie@gmail.com>
2024-01-19 20:00:34 +0100

    Add a dark mode switch

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,7 @@
 import React, {useState, useEffect} from 'react'
 import styles from './Header.module.css'
 import Link from 'next/link'
+import ThemeSwitch from './ThemeSwitch'
 
 const Header = () => {  
   
@@ -13,7 +14,7 @@ const Header = () => {
   const handleClick = () => setIsChecked(false)
 
   useEffect(() => {
-    window.addEventListener('resize', (e) => {
+    window.addEventListener('resize', () => {
       if (window.innerWidth > 640) setIsChecked(false)
     })
   })
@@ -21,7 +22,7 @@ const Header = () => {
 
   return (
     <div className='h-20 flex justify-end static'>       
-      <div className={isChecked ? 'backdrop-blur absolute bottom-0 left-0 size-full' : ''}></div>
+      <div className={isChecked ? 'backdrop-blur backdrop-brightness-25 absolute bottom-0 left-0 size-full' : ''}></div>
       <div className={`${ isChecked ? 'absolute left-0' : ''} my-4 ml-4 mr-auto`}>
         <Link className='text-4xl text-pale-yellow' href='/' onClick={handleClick}>RB</Link>
       </div> 
@@ -31,14 +32,17 @@ const Header = () => {
           {/* this hidden checkbox controls the state */}
             <input type="checkbox" onChange={handleChange} checked={isChecked}/>          
             {/* <!-- hamburger icon --> */}
-            <svg className={["swap-off ", styles.grapefruit].join(' ')} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
+            <svg className="swap-off fill-grapefruit" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
             {/* <!-- close icon --> */}
-            <svg className={["swap-on ", styles.grapefruit].join(' ')} xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>  
+            <svg className="swap-on fill-grapefruit" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>  
         </label>
-        <nav className={`${ isChecked ? 'flex flex-col items-end' : 'hidden'} h-screen sm:flex sm:h-full bg-cool-blue `}> 
+        <nav className={`${ isChecked ? 'flex flex-col items-end' : 'hidden'} sm:flex sm:h-full `}> 
             <Link className='subtitle m-4 transition-all hover:scale-110' href={'/projets'} onClick={handleClick}>PROJETS</Link>
             <Link className='subtitle m-4 transition-all hover:scale-110' href={'/competences'} onClick={handleClick}>COMPÉTENCES</Link>
-            <Link className='subtitle m-4 transition-all hover:scale-110' href={'/contact'} onClick={handleClick}>CONTACT</Link>
+            <Link className='subtitle m-4 transition-all hover:scale-110' href={'/contact'} onClick={handleClick}>CONTACT</Link>            
+            <div>
+              <ThemeSwitch />
+            </div>
         </nav>  
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto_Condensed } from 'next/font/google';
 import './globals.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { Providers } from './providers'
 
 const roboto = Roboto_Condensed({ subsets: ['latin'] })
 
@@ -16,13 +17,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={`${roboto.className} min-h-screen flex flex-col`}>
-        <Header />
-        {children}
-        <Footer />
-        </body>
-    </html>
+  return (    
+    //suppressHydrationWarning necessary with Next Theme so React doen't send message if server and client render different content
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${roboto.className} min-h-screen flex flex-col`}>
+          <Providers>
+            <Header />
+            {children}
+            <Footer />
+          </Providers>
+          </body>
+      </html>    
   )
 }
