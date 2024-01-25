dde2eabc815ed4c302db01505a56b9aaf3aa4375
Benido <remi.besnie@gmail.com>
2024-01-25 16:14:24 +0100

    Remove apostrophes that were triggering ESLINT

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import styles from './Home.module.css';
 
 export default function Home() {
   return (
-    <main className="container container-flex-centered">
+    <main className="container flex grow flex-col items-start px-4 py-24 mx-auto">
       <div className='flex flex-col items-start justify-between'>
         <h1 className='title leading-big-title my-5'>Rémi Besnié</h1>
         <h2 className='subtitle my-1'>Bonjour !</h2>
-        <p className='text mb-3'>Je suis Rémi BESNIÉ, développeur web junior depuis 2024. J’ai effectué une reconversion professionnelle en 2022-2023 auprès de l’école en ligne Studi et j’ai décroché mon titre professionnel en Octobre 2023</p>
+        <p className='text mb-3'>Je suis Rémi BESNIÉ, développeur web junior depuis 2024. J&apos;ai effectué une reconversion professionnelle en 2022-2023 auprès de l&apos;école en ligne Studi et j&apos;ai décroché mon titre professionnel en Octobre 2023</p>
       </div>
     </main>
   )
--- app/projets/page.tsx
+++ app/projets/page.tsx
@@ -10,8 +10,8 @@ export default function Projets() {
         <div className='flex flex-col md:flex-row my-5'>
           <div className='my-0 md:my-4 me-8 max-w-md'>
             <h2 className="subtitle mb-1">Quai Antique</h2>
-            <p className="text mb-3">Projet final du cursus développeur web chez Studi, présenté pour l'oral du titre professionnel.
-            Quai Antique est un restaurant (fictif) pour lequel j'ai réalisé un site présentant l'établissement, publiant les menus et la carte, et permettant la réservation de table ainsi que la création de compte.</p>
+            <p className="text mb-3">Projet final du cursus développeur web chez Studi, présenté pour l&apos;oral du titre professionnel.
+            Quai Antique est un restaurant (fictif) pour lequel j&apos;ai réalisé un site présentant l&apos;établissement, publiant les menus et la carte, et permettant la réservation de table ainsi que la création de compte.</p>
             <p>Back-end : Symfony - MariaDB</p>
             <p>Front-end : Twig - Stimulus - jQuery - Bootstrap</p>
           </div>        
@@ -37,7 +37,7 @@ export default function Projets() {
         <div className='flex flex-col md:flex-row my-8'>
           <div className='my-0 md:my-4 me-8 max-w-md'>
             <h2 className="subtitle mb-1">Laura Chaponot - restauratrice de céramique</h2>
-            <p className="text mb-3">Site vitrine pour une restauratrice d'oeuvres d'art</p>
+            <p className="text mb-3">Site vitrine pour une restauratrice d&apos;oeuvres d&apos;art</p>
           </div>        
           <Link href='' rel='noopener noreferrer' target='_blank'>
             <div className='relative object-cover h-48 md:h-64 w-64 md:w-96 my-4 mx-auto text-center transition-all hover:scale-110'>
